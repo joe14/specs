b54832936c1e5844f7c9e4a317af6f3d938bf640
Joe Dougherty <joe14@commsat.com>
2014-02-21 18:15:05 -0500

    Added examples for vsipl_dda_dkrequired_buffer_size_p(), vsip_dda_dksync_in_p() and vsip_dda_dksync_out_p(); Fixed a typo in the DDA section on aliasing.

--- vsipl/examples/dda_dkrequired_buffer_size_p.c
+++ vsipl/examples/dda_dkrequired_buffer_size_p.c
@@ -1,1 +1,21 @@
- 
+#include "stdlib.h"
+#include "vsip.h"
+#define N 1024
+int main(int argc, char **argv)
+{
+    vsip_vview_f *vw;
+    vsip_scalar_f *buf = 0;
+    vsip_dda_sync_policy pol = VSIP_DDA_SYNC_POLICY_IN;
+    vsip_length reqsz;
+    vsip_dda_vdata_f *data;
+    vsip_init((void*)0);
+    if(NULL == (vw = vsip_vcreate_f(N, VSIP_MEM_NONE))) return -1;
+    if(0 != (reqsz = vsip_dda_vrequired_buffer_size_f(vw, 0, pol)))
+        buf = malloc(reqsz * sizeof(vsip_scalar_f));
+    if(NULL == (data = vsip_dda_vdatacreate_f(vw, 0, pol, buf))) return -1;
+    /* operate on the data object */
+    if(0 != vsip_dda_vdatadestroy_f(data)) return -1;
+    vsip_valldestroy_f(vw);
+    vsip_finalize((void*)0);
+    return 0;
+}
--- vsipl/examples/dda_dksync_out_p.c
+++ vsipl/examples/dda_dksync_out_p.c
@@ -1,1 +1,21 @@
- 
+#include "stdlib.h"
+#include "vsip.h"
+#define N 1024
+int main(int argc, char **argv)
+{
+    vsip_vview_f *vw;
+    vsip_dda_sync_policy pol = VSIP_DDA_SYNC_POLICY_OUT;
+    vsip_dda_vdata_f *data;
+    vsip_scalar_f *ptr;
+    vsip_init((void*)0);
+    if(NULL == (vw = vsip_vcreate_f(N, VSIP_MEM_NONE))) return -1;
+    if(NULL == (data = vsip_dda_vdatacreate_f(vw, 0, pol, 0))) return -1;
+    if(NULL == (ptr = vsip_dda_vptr_f(data))) return -1;
+    vsip_vput_f(vw, 0, 0.0F); ptr[0] = 1.0F; /* vsip_vget_f(vw, 0) == 0.0 */
+    if(0 != (vsip_dda_vsync_out_f(data))) return -1;
+    /* vsip_vget_f(vw, 0) == 1.0 */
+    if(0 != vsip_dda_vdatadestroy_f(data)) return -1;
+    vsip_valldestroy_f(vw);
+    vsip_finalize((void*)0);
+    return 0;
+}
