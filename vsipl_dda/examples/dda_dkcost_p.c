6a18d98268fe967734958a532945c92457b69cfc
Joe Dougherty <joseph.dougherty@ngc.com>
2014-02-20 19:04:47 -0500

    Marked as version 0.14. Moved vsip_cmplx_mem back to section 2.4, along with its deprecation warning; Added a note to vsip_storage_format regarding how the current literal definitions will be relaxed in a future version; Added the static keyword to vsip_dim_order and vsip_dda_sync_policy constant definitions to make them valid C constants; Assigned VSIP_STORAGE_FORMAT_ANY a value so that it would not conflict with VSIP_STORAGE_FORMAT_INTERLEAVED_COMPLEX; Added a note to vsip_dda_dkdatadestroy_p() regarding destruction of VSIPL space DDA buffers; Re-ordered the function sections; Collapsed the complex pointer type function definitions by adding the replaceable character q; Changed the word "interleaved" in function names to "inter"; Added a description of the VSIP_STORAGE_FORMAT_ANY storage format; Compressed the examples; Added referenences from empty examples sections to function sections that contain relevant examples.

--- vsipl_dda/examples/dda_dkcost_p.c
+++ vsipl_dda/examples/dda_dkcost_p.c
@@ -1,34 +1,19 @@
+#include "stdlib.h"
 #include "vsip.h"
-#include "stdio.h"
-
 #define N 1024
-
 int main(int argc, char **argv)
 {
     vsip_cvview_f *vw;
     vsip_datalayout lyt;
-    int cost;
-    
+    int cost;        
     vsip_init((void *)0);
- 
-    vw = vsip_cvcreate_f(N, VSIP_MEM_NONE);
-    if(NULL == vw)
-    {
-        printf("ERROR: Could not allocate the view\n");
-        return -1;
-    }
-
+    if (NULL == (vw = vsip_cvcreate_f(N, VSIP_MEM_NONE))) return -1;
     lyt.dim = 1;
     lyt.storage_format = VSIP_STORAGE_FORMAT_ARRAY;
     lyt.packing = VSIP_PACK_TYPE_DENSE;
     lyt.order = VSIP_DIM_ORDER_VROW_MAJOR;
-
-    cost = vsip_dda_cvcost_f(vw, &lyt, VSIP_DDA_SYNC_POLICY_INOUT);
-    printf("Cost = %d\n", cost);
-    
-    vsip_cvalldestroy_f(vw);
-    
-    vsip_finalize((void *)0);
-    
+    cost = vsip_dda_cvcost_f(vw, &lyt, VSIP_DDA_SYNC_POLICY_INOUT); 
+    vsip_cvalldestroy_f(vw);    
+    vsip_finalize((void *)0);   
     return 0;
 } 
--- vsipl_dda/examples/dda_kptr_p.c
+++ vsipl_dda/examples/dda_kptr_p.c
@@ -1,1 +1,26 @@
- 
+#include "stdlib.h"
+#include "vsip.h"
+#define N 1024
+int main(int argc, char **argv)
+{
+    vsip_vview_i *vw;
+    vsip_dda_sync_policy pol;
+    vsip_dda_vdata_i *data;
+    vsip_scalar_i *ptr;
+    vsip_init((void*)0);
+    if(NULL == (vw = vsip_vcreate_i(N, VSIP_MEM_NONE))) return -1;
+    vsip_vput_i(vw, 0, 1); /* vsip_vget_i(vw, 0) == 1 */
+    pol = VSIP_DDA_SYNC_POLICY_INOUT | VSIP_DDA_SYNC_POLICY_COPY;
+    /* NOTE
+     * With this synchronization policy, a DDA buffer will be created even if
+     * the implementation would not have created one otherwise.
+     */
+    if(NULL == (data = vsip_dda_vdatacreate_i(vw, 0, pol, 0))) return -1;
+    if(NULL == (ptr = vsip_dda_vptr_i(data))) return -1;
+    ptr[0] += 1;
+    if(0 != vsip_dda_vdatadestroy_i(data)) return -1;
+    /* vsip_vget_i(vw, 0) == 2 */
+    vsip_valldestroy_i(vw);
+    vsip_finalize((void*)0);
+    return 0;
+}
